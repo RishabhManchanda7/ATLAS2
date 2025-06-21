3b1e4c270db5e079c4dbee4ba2eefdbb36b0e4a2
RishabhManchanda7 <manchanda.rishabh90@gmail.com>
2025-06-21 19:11:04 +0530

    DAY-11 , file handling task

---
+++ src/renamefile.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class renamefile {
+    public static void main(String[] args) {
+        File f1 = new File(args[0]);
+        File f2  = new File(args[1]);
+        f1.renameTo(f2);
+        System.out.println("rename file " + f1 + "to" + f2 + "sucessfully");
+    }
+}
