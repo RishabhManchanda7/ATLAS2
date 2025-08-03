f9e52a72e71a673e0d7123210cc1ddd76ad28aa7
RishabhManchanda7 <mrishabbh123@gmail.com>
2025-08-04 00:38:05 +0530

    Day-22

---
+++ src/Day22/MyTest.java
@@ -0,0 +1,12 @@
+package Day22;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MyTest {
+
+    @Test
+    void testAddition(){
+        assertEquals(4,2+2);
+    }
+}
